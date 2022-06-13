4e9a107f6620104cd6d7dbb4aa6fa4d0dbc6cb34
Luiz Fernando Paes <79661911+Luizfpbp@users.noreply.github.com>
2022-06-13 19:00:32 -0300

    brabo

--- GreenRoots01/src/repositorio/Mediador.java
+++ GreenRoots01/src/repositorio/Mediador.java
@@ -23,48 +23,86 @@ public class Mediador {
 		
 	}
 	
-	public void cadastro(String nome, Date nas, String email, String cell, String senha) {
+	public boolean cadastro(String nome, Date nas, String email, String cell, String senha) {
 		
 		boolean mailV = val.isValidEmail(email);
 		boolean cellV = val.isValidPhone(cell);
 		boolean dataV = val.isValidDate(nas);
 		boolean nameV = val.isValidName(nome);
-		boolean passV = val.isValidName(senha);
+		boolean passV = val.isValidPass(senha);
 		
 		if (!mailV) {
 			System.out.println("E-mail invalido!");
-			return;
+			return false;
 		}
 		
-		if (!cellV) {
-			System.out.println("Telefone invalido!");
-			return;
+		if (!cellV) {                                                
+			System.out.println("Telefone invalido! formato correto ex: 819******** ");
+			return false;
 		}
 		
 		if (!dataV) {
 			System.out.println("Data de nascimento invalida!");
-			return;
+			return false;
 		}
 		
 		if (!nameV) {
 			System.out.println("Nome invalido!");
-			return;
+			return false;
 		}
 		
 		if (!passV) {
 			System.out.println("Senha invalida!");
-			return;
+			return false;
 		}
 		
 		Usuario acc = new Usuario(nome, nas, email, cell, senha);
 		
-		if (rep.addConta(acc) == 1) {
+		if (rep.addConta(acc)) {
 			System.out.println("Conta adicionada com sucesso!");
+			
 		}
 		else {
 			System.out.println("Conta ja adicionada!");
 		}
 		
+		return true;
+		
+	}
+	
+	public void perfil (Usuario conta) {
+		rep.printPerfil(conta);
+		
+	}
+	
+	public boolean alteraSenha(Usuario conta, String senhaAtual, String novaSenha){
+		if (rep.comparaSenha(conta, senhaAtual)){
+			if(val.isValidPass(novaSenha)){
+				rep.alteraSenha(conta, novaSenha);
+				return true;
+			}
+			else {
+				System.out.println("Nova senha incorreta! Formato correto: Maior que 5 caracteres e pelo menos uma maiúscula");
+				return false;
+			}
+		}
+		System.out.println("Senha atual incorreta");
+		return false;
+	}
+	
+	public boolean alteraNome(Usuario conta, String nome, String senha){
+		if (rep.comparaSenha(conta, senha)){
+			if(val.isValidName(nome)){
+				rep.alteraNome(conta, nome);
+				return true;
+			}
+			else {
+				System.out.println("Nome invalido!");
+				return false;
+			}
+		}
+		System.out.println("Senha incorreta");
+		return false;
 	}
 	
 	
